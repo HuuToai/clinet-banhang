842e9119ba6e965289ce298f3b1387d583e91ece
Huutoai <71DCHT22129@student.utt.vn>
2023-12-07 09:46:28 +0700

    Accountadmin

--- src/app/middleWare/basevalidator.js
+++ src/app/middleWare/basevalidator.js
@@ -1,10 +1,11 @@
 function checkvalidateCreateBrand(req, user) {
-  req.checkBody("email", "Email is required").notEmpty(); //validate để trống trường email sử dụng hàm notEmpty()
-  return req.validationErrors();
+    req.checkBody('email', 'Email is required').notEmpty(); //validate để trống trường email sử dụng hàm notEmpty()
+    return req.validationErrors();
 }
 
 module.exports = {
-  checkvalidateCreateBrand: checkvalidateCreateBrand,
+    checkvalidateCreateBrand: checkvalidateCreateBrand,
 };
 
 // comment
+//comment 2
